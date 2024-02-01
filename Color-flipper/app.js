ff94d54f3f8f6621e4fed1487498c434a63f96c8
Suleman Khalil <sulemankhalil44@gmail.com>
2024-02-01 11:49:35 +0500

    deleted the console log

--- Color-flipper/app.js
+++ Color-flipper/app.js
@@ -6,7 +6,6 @@ const color = document.querySelector('.color');
 btn.addEventListener("click", function(){
    //get Random number between 0 and 3 
    const randomNumber = getRandomNumber();
-   console.log(randomNumber);
    document.body.style.backgroundColor = colors[randomNumber];
    color.textContent = colors[randomNumber];
 })
